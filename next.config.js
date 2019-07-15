573efa920100ba3d9404b42774db8dafb7514407
马文杰 <mawenjie@dwnews.com>
2019-07-15 11:30:23 +0800

    add configuration for svg images

--- next.config.js
+++ next.config.js
@@ -17,5 +17,18 @@ module.exports = withLess({
   },
   antdLessLoaderOptions: {
     javascriptEnabled: true
+  },
+  // eslint-disable-next-line no-unused-vars
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000
+        }
+      }
+    });
+    return config;
   }
 });
